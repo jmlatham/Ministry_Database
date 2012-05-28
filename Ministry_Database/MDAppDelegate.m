a665b1ba78c0ae20422560dd04e7c35c5cacb6f7
Marshall Latham <jmlatham@yahoo.com>
2012-05-28 10:34:43 -0500

    adjusted the Hello World label text

--- Ministry_Database/MDAppDelegate.m
+++ Ministry_Database/MDAppDelegate.m
@@ -27,7 +27,7 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
-    [self.helloWorldLabel setStringValue:@"Hello World"];
+    [self.helloWorldLabel setStringValue:@"Hello World-1"];
 }
 
 /**
